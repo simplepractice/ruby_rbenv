57f113434ca9a48e0c4efdac847848ce33eda2b9
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2017-10-23 17:50:08 +0300

    Run gem resource commands as a user

--- resources/gem.rb
+++ resources/gem.rb
@@ -110,6 +110,10 @@ action_class do
   include Chef::Rbenv::ScriptHelpers
 
   def binary
-    "#{root_path}/versions/#{new_resource.rbenv_version}/bin/gem"
+    prefix = if new_resource.user
+      "sudo -u #{new_resource.user}"
+    end
+
+    "#{prefix} #{root_path}/versions/#{new_resource.rbenv_version}/bin/gem"
   end
 end
